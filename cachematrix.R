7749260a05fb6dee39a0150327abd03ca0b4beb2
ManishSoni67 <manish.6709@gmail.com>
2016-07-23 03:58:29 -0500

    added comments in the R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix will take a matrix object and new object by default and will
+## return a list of 4 methods i.e setmatrix,setmatrix,getinv,setinv and all these
+## methods are nested methods with lexical scope variable i.e invert 
+## which will hold the inverted matrix for matrix say x.
 
 makeCacheMatrix <- function(x = matrix()) {
   inversion <- NULL
-  setmatrix <- function(y) {
+  set <- function(y) {
     x <<- solve(y)
     inversion <<- NULL
   }
-  getmatrix <- function() x
+  get <- function() x
   setinversion <- function(invert) inversion <<- invert
   getinversion <- function() inversion
-  list(set = setmatrix, get = getmatrix,
+  list(setmatrix = set, getmatrix = get,
        setinv = setinversion,
        getinv = getinversion)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve method will take function makeCacheMatrix as argument where the list 
+## of methods will be called for inverting the (makeCacheMatrix(x)), 
+## x matrix variable, and if the value is not cached i.e (is.null(inv))==TRUE
+## we will set the inverted matrix and cache it in the setinv nested function
+## body and we return its body
+ 
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
@@ -27,7 +32,7 @@ cacheSolve <- function(x) {
     message("getting cached inversion data")
     return(inv)
   }
-  data <- x$get()
+  data <- x$getmatrix()
   inv <- solve(data)
   x$setinv(inv)
   inv
